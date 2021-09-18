506dbbcfa8604b29ba5190d46f3ddc1c3ddfbd89
Antonio C. Jr <acdcjunior@gmail.com>
2021-09-18 17:15:58 -0300

    refactor(home): move NewReviewModal subcomponents out of main function

--- app/javascript/components/NewReviewModalComponent.jsx
+++ app/javascript/components/NewReviewModalComponent.jsx
@@ -5,132 +5,134 @@ import {$starOff, $starOn} from "./StarSvg";
 
 const SHAKE_EFFECT_CLASSNAME = 'shake';
 
+
+function SelectRatingJQueryComponent($reviewStarsRoot) {
+    const $stars = $reviewStarsRoot.children();
+    let selectedRating = 0;
+
+    const selectNewRating = (rating) => {
+        if (selectedRating === rating) {
+            selectedRating = 0;
+        } else {
+            selectedRating = rating;
+        }
+        $stars.attr('aria-pressed', 'false')
+            .slice(0, selectedRating).attr('aria-pressed', 'true')
+    }
+    const resetSelectedRating = () => {
+        selectNewRating(0);
+        resetHighlightedStars();
+    };
+    const highlightStarsUpTo = (rating) => {
+        $stars.removeClass('star-on')
+            .slice(0, rating).addClass('star-on')
+    }
+    const resetHighlightedStars = () => {
+        highlightStarsUpTo(selectedRating);
+    }
+    const flashInvalid = () => {
+        $reviewStarsRoot.addClass(SHAKE_EFFECT_CLASSNAME);
+        setTimeout(() => $reviewStarsRoot.removeClass(SHAKE_EFFECT_CLASSNAME), 300);
+    }
+    const initView = () => {
+        $reviewStarsRoot.children('span').append([$starOn.clone(), $starOff.clone()]);
+    }
+    const bindEventsOnView = () => {
+        $stars.each((i, e) => {
+            const ratingForThisStar = i + 1;
+            $(e).on({
+                mouseenter: () => highlightStarsUpTo(ratingForThisStar),
+                mouseleave: resetHighlightedStars,
+                click: () => selectNewRating(ratingForThisStar)
+            });
+        });
+    }
+
+    initView();
+    bindEventsOnView();
+    return {
+        get selectedRating() {
+            return selectedRating;
+        },
+        resetSelectedRating,
+        flashInvalid
+    };
+}
+
+
+function ReviewTextAreaJQueryComponent($reviewTextArea) {
+    const flashInvalidAndBringFocus = () => {
+        $reviewTextArea.parent().addClass(SHAKE_EFFECT_CLASSNAME);
+        setTimeout(() => $reviewTextArea.parent().removeClass(SHAKE_EFFECT_CLASSNAME), 300);
+        $reviewTextArea.focus();
+    }
+    const resetTypedReview = () => {
+        $reviewTextArea.text('')
+    }
+    const getTypedReviewText = () => $reviewTextArea.text().trim();
+
+    $reviewTextArea.blur(() => {
+        // workaround contenteditable bug that does not show placeholder after some multi-line text has been entered and deleted
+        if (getTypedReviewText() === '') {
+            resetTypedReview();
+        }
+    });
+
+    return {
+        get typedReview() {
+            return getTypedReviewText();
+        },
+        resetTypedReview,
+        flashInvalidAndBringFocus,
+        focusTextArea: () => {
+            $reviewTextArea.focus();
+            if (getTypedReviewText() !== '') {
+                // if text exists, then the modal has been shown and was closed without a submit...
+                // ...so, select previously typed text to make it easier for the user to re-type
+                window.getSelection().selectAllChildren($reviewTextArea[0]);
+            }
+        }
+    };
+}
+
+function ModalJQuery($reviewModal, reviewTextAreaComponent) {
+    const openModal = () => {
+        $reviewModal.show();
+        reviewTextAreaComponent.focusTextArea();
+    }
+    const hideModal = () => {
+        $reviewModal.hide();
+    }
+    const handleClickOutsideModal = () => {
+        $(window).mousedown((event) => {
+            if (event.target === $reviewModal[0]) {
+                hideModal();
+            }
+        });
+    };
+    const handleEscapeKeyPress = () => {
+        $(document).keyup(function (e) {
+            if (e.key === 'Escape') {
+                hideModal();
+            }
+        });
+    };
+
+    const bindEvents = () => {
+        handleClickOutsideModal();
+        handleEscapeKeyPress();
+    }
+    bindEvents();
+
+    return {
+        openModal,
+        hideModal
+    }
+}
+
 export function NewReviewModalJQueryComponent(
     $newReviewModal, $newReviewStarsRoot, $newReviewReviewTextArea, $newReviewSubmitButton, $newReviewSubmitBlock, onNewReviewSaved
 ) {
-
-    function SelectRatingComponent($reviewStarsRoot) {
-        const $stars = $reviewStarsRoot.children();
-        let selectedRating = 0;
-
-        const selectNewRating = (rating) => {
-            if (selectedRating === rating) {
-                selectedRating = 0;
-            } else {
-                selectedRating = rating;
-            }
-            $stars.attr('aria-pressed', 'false')
-                .slice(0, selectedRating).attr('aria-pressed', 'true')
-        }
-        const resetSelectedRating = () => {
-            selectNewRating(0);
-            resetHighlightedStars();
-        };
-        const highlightStarsUpTo = (rating) => {
-            $stars.removeClass('star-on')
-                .slice(0, rating).addClass('star-on')
-        }
-        const resetHighlightedStars = () => {
-            highlightStarsUpTo(selectedRating);
-        }
-        const flashInvalid = () => {
-            $reviewStarsRoot.addClass(SHAKE_EFFECT_CLASSNAME);
-            setTimeout(() => $reviewStarsRoot.removeClass(SHAKE_EFFECT_CLASSNAME), 300);
-        }
-        const initView = () => {
-            $reviewStarsRoot.children('span').append([$starOn.clone(), $starOff.clone()]);
-        }
-        const bindEventsOnView = () => {
-            $stars.each((i, e) => {
-                const ratingForThisStar = i + 1;
-                $(e).on({
-                    mouseenter: () => highlightStarsUpTo(ratingForThisStar),
-                    mouseleave: resetHighlightedStars,
-                    click: () => selectNewRating(ratingForThisStar)
-                });
-            });
-        }
-
-        initView();
-        bindEventsOnView();
-        return {
-            get selectedRating() {
-                return selectedRating;
-            },
-            resetSelectedRating,
-            flashInvalid
-        };
-    }
-
-    function ReviewTextAreaComponent($reviewTextArea) {
-        const flashInvalidAndBringFocus = () => {
-            $reviewTextArea.parent().addClass(SHAKE_EFFECT_CLASSNAME);
-            setTimeout(() => $reviewTextArea.parent().removeClass(SHAKE_EFFECT_CLASSNAME), 300);
-            $newReviewReviewTextArea.focus();
-        }
-        const resetTypedReview = () => {
-            $reviewTextArea.text('')
-        }
-        const getTypedReviewText = () => $reviewTextArea.text().trim();
-
-        $reviewTextArea.blur(() => {
-            // workaround contenteditable bug that does not show placeholder after some multi-line text has been entered and deleted
-            if (getTypedReviewText() === '') {
-                resetTypedReview();
-            }
-        });
-
-        return {
-            get typedReview() {
-                return getTypedReviewText();
-            },
-            resetTypedReview,
-            flashInvalidAndBringFocus,
-            focusTextArea: () => {
-                $reviewTextArea.focus();
-                if (getTypedReviewText() !== '') {
-                    // if text exists, then the modal has been shown and was closed without a submit...
-                    // ...so, select previously typed text to make it easier for the user to re-type
-                    window.getSelection().selectAllChildren($reviewTextArea[0]);
-                }
-            }
-        };
-    }
-
-    function Modal($reviewModal, reviewTextAreaComponent) {
-        const openModal = () => {
-            $reviewModal.show();
-            reviewTextAreaComponent.focusTextArea();
-        }
-        const hideModal = () => {
-            $reviewModal.hide();
-        }
-        const handleClickOutsideModal = () => {
-            $(window).mousedown((event) => {
-                if (event.target === $reviewModal[0]) {
-                    hideModal();
-                }
-            });
-        };
-        const handleEscapeKeyPress = () => {
-            $(document).keyup(function (e) {
-                if (e.key === 'Escape') {
-                    hideModal();
-                }
-            });
-        };
-
-        const bindEvents = () => {
-            handleClickOutsideModal();
-            handleEscapeKeyPress();
-        }
-        bindEvents();
-
-        return {
-            openModal,
-            hideModal
-        }
-    }
 
     const postNewReview = review => $.ajax({
         type: 'POST',
@@ -141,7 +143,6 @@ export function NewReviewModalJQueryComponent(
             authenticity_token: $('[name="csrf-token"]').attr('content')
         }
     });
-    window.postNewReview = postNewReview;
 
     const validRatings = [1, 2, 3, 4, 5];
     const validateNewReview = ({ rating, review }) => {
@@ -208,9 +209,9 @@ export function NewReviewModalJQueryComponent(
     };
 
     function init() {
-        const selectedRatingModel = SelectRatingComponent($newReviewStarsRoot);
-        const reviewTextAreaComponent = ReviewTextAreaComponent($newReviewReviewTextArea);
-        const modal = Modal($newReviewModal, reviewTextAreaComponent);
+        const selectedRatingModel = SelectRatingJQueryComponent($newReviewStarsRoot);
+        const reviewTextAreaComponent = ReviewTextAreaJQueryComponent($newReviewReviewTextArea);
+        const modal = ModalJQuery($newReviewModal, reviewTextAreaComponent);
 
         bindSubmitClick(selectedRatingModel, reviewTextAreaComponent, modal);
 
