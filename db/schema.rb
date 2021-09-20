374ea7eb6007fb1f81a87d767eebb5ae79d5d37a
Antonio C. Jr <acdcjunior@gmail.com>
2021-09-20 13:42:17 -0300

    feat(reviews): add product foreign key
    (cherry picked from commit 6ef3bac1fdf8c828f26ca3cd40b6f282117f83bc)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_20_132514) do
+ActiveRecord::Schema.define(version: 2021_09_20_162756) do
 
   # original collation below was 'utf8mb4_0900_ai_ci', but we have to change it to utf8mb4_unicode_ci due to ClearDB@Heroku
   # otherwise `$ heroku run 'rake db:setup'` throws `Mysql2::Error: Unknown collation: 'utf8mb4_0900_ai_ci'`
@@ -31,6 +31,9 @@ ActiveRecord::Schema.define(version: 2021_09_20_132514) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "half_star"
+    t.bigint "product_id", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
+  add_foreign_key "reviews", "products"
 end
